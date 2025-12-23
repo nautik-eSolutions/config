b7d49f5d48ee26ea7bee317863806a6a788dc6c8
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-12-23 08:24:57 +0100

    Added company relation, port,city and communtity

--- db/nautik.sql
+++ db/nautik.sql
@@ -18,10 +18,10 @@ SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
 SET @@SESSION.SQL_LOG_BIN= 0;
 
 --
--- GTID state at the beginning of the backup 
+-- GTID state at the beginning of the backup
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'd782dc28-defe-11f0-97f0-9a63c23be361:1-20';
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'd782dc28-defe-11f0-97f0-9a63c23be361:1-35';
 
 --
 -- Table structure for table `admin`
@@ -31,15 +31,73 @@ DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  PRIMARY KEY (`id`,`user_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_port_admin_user_idx` (`user_id`),
-  CONSTRAINT `fk_port_admin_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `user_id` int NOT NULL,
+                         PRIMARY KEY (`id`,`user_id`),
+                         UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+                         UNIQUE KEY `id_UNIQUE` (`id`),
+                         KEY `fk_port_admin_user_idx` (`user_id`),
+                         CONSTRAINT `fk_port_admin_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `city` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `name` varchar(50) NOT NULL,
+                        `community_id` int NOT NULL,
+                        PRIMARY KEY (`id`),
+                        KEY `fk_city` (`community_id`),
+                        CONSTRAINT `fk_city` FOREIGN KEY (`community_id`) REFERENCES `community` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `city`
+--
+
+LOCK TABLES `city` WRITE;
+/*!40000 ALTER TABLE `city` DISABLE KEYS */;
+/*!40000 ALTER TABLE `city` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `community`
+--
+
+DROP TABLE IF EXISTS `community`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `community` (
+                             `id` int NOT NULL AUTO_INCREMENT,
+                             `name` varchar(60) NOT NULL,
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `community`
+--
+
+LOCK TABLES `community` WRITE;
+/*!40000 ALTER TABLE `community` DISABLE KEYS */;
+/*!40000 ALTER TABLE `community` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `company`
@@ -49,14 +107,26 @@ DROP TABLE IF EXISTS `company`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `company` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `vat` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) NOT NULL,
+                           `vat` varchar(20) NOT NULL,
+                           `email` varchar(100) NOT NULL,
+                           `phone` varchar(100) NOT NULL,
+                           `admin` int NOT NULL,
+                           PRIMARY KEY (`id`),
+                           KEY `fk_company_1_idx` (`admin`),
+                           CONSTRAINT `fk_company_1` FOREIGN KEY (`admin`) REFERENCES `company_admin` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `company_admin`
@@ -66,13 +136,53 @@ DROP TABLE IF EXISTS `company_admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `company_admin` (
-  `id` int NOT NULL,
-  `admin_id` int NOT NULL,
-  PRIMARY KEY (`id`,`admin_id`),
-  KEY `fk_company_admin_admin_idx` (`admin_id`),
-  CONSTRAINT `fk_company_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+                                 `admin_id` int NOT NULL,
+                                 `id` int NOT NULL,
+                                 PRIMARY KEY (`admin_id`,`id`),
+                                 UNIQUE KEY `admin_id_UNIQUE` (`admin_id`),
+                                 UNIQUE KEY `id_UNIQUE` (`id`),
+                                 KEY `fk_company_admin_admin_idx` (`admin_id`),
+                                 CONSTRAINT `fk_company_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company_admin`
+--
+
+LOCK TABLES `company_admin` WRITE;
+/*!40000 ALTER TABLE `company_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `port`
+--
+
+DROP TABLE IF EXISTS `port`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `port` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `name` varchar(255) NOT NULL,
+                        `city_id` int NOT NULL,
+                        `company_id` int NOT NULL,
+                        PRIMARY KEY (`id`),
+                        KEY `fk_port_company` (`company_id`),
+                        KEY `fk_port_city_id` (`city_id`),
+                        CONSTRAINT `fk_port_city_id` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                        CONSTRAINT `fk_port_company` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `port`
+--
+
+LOCK TABLES `port` WRITE;
+/*!40000 ALTER TABLE `port` DISABLE KEYS */;
+/*!40000 ALTER TABLE `port` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `port_admin`
@@ -82,13 +192,51 @@ DROP TABLE IF EXISTS `port_admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `port_admin` (
-  `id` int NOT NULL,
-  `admin_id` int NOT NULL,
-  PRIMARY KEY (`id`,`admin_id`),
-  KEY `fk_port_admin_1_idx` (`admin_id`),
-  CONSTRAINT `fk_port_admin_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+                              `id` int NOT NULL,
+                              `admin_id` int NOT NULL,
+                              PRIMARY KEY (`id`,`admin_id`),
+                              UNIQUE KEY `id` (`id`),
+                              KEY `fk_port_admin_1_idx` (`admin_id`),
+                              CONSTRAINT `fk_port_admin_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `port_admin`
+--
+
+LOCK TABLES `port_admin` WRITE;
+/*!40000 ALTER TABLE `port_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `port_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `port_admin_port`
+--
+
+DROP TABLE IF EXISTS `port_admin_port`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `port_admin_port` (
+                                   `id` int NOT NULL AUTO_INCREMENT,
+                                   `port_id` int NOT NULL,
+                                   `port_admin_id` int NOT NULL,
+                                   PRIMARY KEY (`id`),
+                                   KEY `fk_port_interr_port` (`port_id`),
+                                   KEY `fk_port_admin` (`port_admin_id`),
+                                   CONSTRAINT `fk_port_admin` FOREIGN KEY (`port_admin_id`) REFERENCES `port_admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                   CONSTRAINT `fk_port_interr_port` FOREIGN KEY (`port_id`) REFERENCES `port` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `port_admin_port`
+--
+
+LOCK TABLES `port_admin_port` WRITE;
+/*!40000 ALTER TABLE `port_admin_port` DISABLE KEYS */;
+/*!40000 ALTER TABLE `port_admin_port` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `super_admin`
@@ -98,14 +246,23 @@ DROP TABLE IF EXISTS `super_admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `super_admin` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `admin_id` int NOT NULL,
-  PRIMARY KEY (`id`,`admin_id`),
-  UNIQUE KEY `admin_id_UNIQUE` (`admin_id`),
-  KEY `fk_super_admin_admin_idx` (`admin_id`),
-  CONSTRAINT `fk_super_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+                               `id` int NOT NULL AUTO_INCREMENT,
+                               `admin_id` int NOT NULL,
+                               PRIMARY KEY (`id`,`admin_id`),
+                               UNIQUE KEY `admin_id_UNIQUE` (`admin_id`),
+                               KEY `fk_super_admin_admin_idx` (`admin_id`),
+                               CONSTRAINT `fk_super_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `super_admin`
+--
+
+LOCK TABLES `super_admin` WRITE;
+/*!40000 ALTER TABLE `super_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `super_admin` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -115,14 +272,23 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `first_name` varchar(45) NOT NULL,
+                        `last_name` varchar(45) NOT NULL,
+                        `email` varchar(255) NOT NULL,
+                        `password` varchar(255) NOT NULL,
+                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -134,4 +300,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-22  9:07:01
+-- Dump completed on 2025-12-23  8:22:34
